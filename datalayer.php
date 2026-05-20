a75bb25d04836474a86069de8febfc64606d919f
Ronan HELLO <ronan56.hello@gmail.com>
2026-05-20 18:30:52 +0200

    update: Tested with WP 7

--- datalayer.php
+++ datalayer.php
@@ -25,7 +25,7 @@ if ( ! function_exists( 'add_action' ) ) {
 // Constant
 define( 'OMDL_VERSION', '1.0.4' );
 define( 'OMDL_PHP_MINIMUM', '8.2.0' );
-define( 'OMDL_WP_MINIMUM', '6.0.0' );
+define( 'OMDL_WP_MINIMUM', '7.0.0' );
 define( 'OMDL_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 require_once OMDL_PLUGIN_DIR . 'inc/datalayer.php';
